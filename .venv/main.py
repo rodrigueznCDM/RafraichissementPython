dfcb4a675d847cec37a3188765facdb248f62269
rodrigueznCDM <rodriguezn@educ.college-montreal.qc.ca>
2024-09-03 10:15:57 -0400

    fin de cours 1

--- .venv/main.py
+++ .venv/main.py
@@ -17,32 +17,48 @@ def division():
         print("entrez un numéro autre que zero la prochaine fois")
         exit("-1")
 
-    produit = numero1 // numero2
+    produit = numero1 / numero2
     print("le résultat de",numero1,"divisé par",numero2,"est de",produit)
 
 exercices = input("quel exercice voulez vous regarder?:\n")
 
 if exercices == "1":
-    print("NOM\n")
+    print("\nNOM\n")
     nom = input("quel est votre nom?:\n")
     print("Bonjour",nom,"Comment allez vous?")
     exit()
 
 elif exercices == "2":
-    print("ADDITION\n")
+    print("\nADDITION\n")
     addition()
     exit()
 
 elif exercices == "3":
-    print("DIVISION\n")
+    print("\nDIVISION\n")
     division()
     exit()
 
 elif exercices == "4":
-    print("")
+    print("\nPAIR OU IMPAIR\n")
+    pair = 0
+    impair = 0
+    for i in range(1,10):
+        typeCheck = i % 2
+        if typeCheck == 0:
+            pair = pair + 1
+            print(i,"est pair")
+
+        else:
+            impair = impair + 1
+            print(i,"est impair")
+
+    print("il y a",pair,"chiffre(s) pair(s) et",impair,"chiffre(s) impair(s)")
+    exit()
 
 elif exercices == "5":
-    print("")
+    print("\nPAIR OU IMPAIR CHOISIR\n")
+
+    exit()
 
 elif exercices == "6":
     print("")
